49dd98b85d73f568ee367883dd4e1aea53f3b2d3
MokaAkasia <rajzona@gmail.com>
2022-02-06 21:57:38 +0300

    Начал создавать uml

---
+++ Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Learning
+{
+    public class Player
+    {
+        private int health;
+        private int weapon;
+        private int box;
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Learning
+{
+    public class Weapon
+    {
+        private int name;
+        private int type;
+        private int ammo;
+    }
+}
